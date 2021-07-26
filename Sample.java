ff8f5484dae1039c079b27243059e8d09283c5f8
jeena-augustine <81424457+jeena-augustine@users.noreply.github.com>
2021-07-26 14:06:21 +0530

    Update and rename Sample.txt to Sample.java

---
+++ Sample.java
@@ -0,0 +1,22 @@
+public class Sample {
+  public static void main(String[] args) {
+
+    // take two numbers
+    double num1 = 10;
+    double num2 = 20;
+
+    // declare sum variable
+    // and initialize with 0
+    double sum = 0.0;
+    // declare average variable
+    double avg = 0.0;
+
+    // calculate the sum value
+    sum = num1 + num2;
+    // calculate the average value
+    avg = sum/2;
+
+    // display result
+    System.out.println("Average: " + avg );
+  }
+}
